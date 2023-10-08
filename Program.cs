4293c60c2d69b38b55037f9bfdd041cfe2645450
IvailoBarchinkov <146868852+IvailoBarchinkov@users.noreply.github.com>
2023-10-08 17:56:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GitHubTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            string word = string.Empty;
+            for (int i = 0; i < num; i++)
+            {
+                word += i;
+            }
+            Console.WriteLine(word + "apwincoainciaopcianwc");
+        }
+        
+    }
+}
